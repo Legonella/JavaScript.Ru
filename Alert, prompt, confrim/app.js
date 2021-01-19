144bbfa717d1115614cbbc08aa8ec9b02257622b
Legonella <dastbravis@gmail.com>
2021-01-19 12:42:52 +0100

    Alert, prompt, confrim

---
+++ Alert, prompt, confrim/app.js
@@ -0,0 +1,12 @@
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+let isBoss = confirm('Are you the boss?');
+
+alert(isBoss); // true if OK is pressed
+
+// Tasks
+let userName = prompt('What is your name?', 'Your name');
+
+alert(`Hello, ${userName}!`);
