fff555929612b88c12b8516141d12da9fa2d52b0
Legonella <dastbravis@gmail.com>
2021-01-21 20:18:13 +0100

    Comparisons, String comparison

---
+++ Comparisons/app.js
@@ -0,0 +1,24 @@
+alert(2 > 1); // true (cerrect)
+alert(2 == 1); //false (wrong)
+alert(2 != 1); // true (correct)
+
+let result = 5 > 4; // assign the result of the comparison
+alert(result); // true
+
+// String comparison
+
+alert('Z' > 'A'); // ture
+alert(' Glow' > 'Glee'); // true
+alert('Bee' > 'Be'); // true
+
+let a = 'b';
+let b = 'B';
+
+if (a > b) {
+    console.log('true');
+} else if (a < b) {
+    console.log('false');
+} else {
+    console.log('nothing');
+}
+
